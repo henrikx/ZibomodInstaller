dbb1b8bc1ec900ce40972153448291539ddb2222
Henrik Takset <00heols@gmail.com>
2017-12-17 21:35:45 +0100

    Bundle Ionic.Zip and build release binaries.

--- ZibomodInstaller/Program.cs
+++ ZibomodInstaller/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Reflection;
 
 namespace ZibomodInstaller
 {
@@ -14,9 +15,20 @@ namespace ZibomodInstaller
         [STAThread]
         static void Main()
         {
+#if !DEBUG
+            Assembly start = Assembly.Load((byte[])Properties.Resources.Ionic_Zip);
+#endif
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
         }
+#if !DEBUG
+        static Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            string assemblyName = new AssemblyName(args.Name).Name.Replace('.', '_');
+            byte[] assemblyBytes = (byte[])Properties.Resources.ResourceManager.GetObject(assemblyName, Properties.Resources.Culture);
+            return Assembly.Load(assemblyBytes);
+        }
+#endif
     }
 }
